bdc2c1a5c11a43053d739fb762b4c873eca76177
Jeffrey Cornillie <jeffrey.cornillie@student.howest.be>
2026-06-04 22:56:38 +0200

    Refactor active bubble code

--- js/helpers/active-bubble.js
+++ js/helpers/active-bubble.js
@@ -10,6 +10,7 @@ let activeBubble;
 let isAnimating = false;
 const animationTime = 0.5;
 
+//#region Initialization
 export function initialize(){
     centerBubble = document.querySelector('.center-bubble');
     if(centerBubble){
@@ -35,7 +36,9 @@ export function initialize(){
         });
     });
 }
+//#endregion Initialization
 
+//#region event handlers
 function onBubbleClick(e){
     if(e.target === activeBubble) return;
     if(activeBubble && activeBubble.contains(e.target)) return;
@@ -64,17 +67,7 @@ function onBubbleClick(e){
     activeBubble.classList.remove('inactive-bubble-right');
     activeBubble.classList.add('activating-bubble');
 
-    const children = activeBubble.querySelectorAll('.bubble-title');
-    const splitPanes = activeBubble.querySelectorAll('.split-pane');
-
-    activeBubble.style.animation = `zoom-in ${animationTime}s ease-out`;
-    children.forEach(child => {
-        child.style.animation = `move-to-top ${animationTime}s ease-out`;
-    });
-    splitPanes.forEach(p => {
-        p.style.animation = `fade-in ${animationTime}s ease-out`;
-        p.style.animationFillMode = 'forwards';
-    });
+    animateOnSetActiveBubble();
     animateSideBubbles();
 
     setTimeout(() => {
@@ -83,38 +76,24 @@ function onBubbleClick(e){
         activeBubble.classList.remove('activating-bubble');
         animateSideBubbles();
         
-        splitPanes.forEach(p => {
-            p.style.animation = '';
-            p.classList.remove('hidden');
-        });
         CARDS.activateBubble(activeBubble);
         clearAnimations();
         isAnimating = false;
     }, animationTime * 1000);
-
-    const divOverlay = document.createElement('div');
-    divOverlay.classList.add('overlay');
-    document.body.appendChild(divOverlay);
-    divOverlay.addEventListener('click', onActiveBubbleClick);
+    createOverlay();
 }
-
-
 function onActiveBubbleClick(e, resetSideBubbles = true, overrideAnimatingCheck = false){
     if(isAnimating && !overrideAnimatingCheck) return;
     isAnimating = true;
     CARDS.deactivateBubble(activeBubble);
     activeBubble.classList.remove('active-bubble');
     activeBubble.classList.remove('blur');
-    e.currentTarget.remove();
+    removeOverlay();
     clearAnimations();
     if(activeBubble === centerBubble)
         activeBubble.style.animation = `zoom-back ${animationTime}s ease-out`;
     else
         activeBubble.style.animation = `zoom-back ${animationTime}s ease-out`;
-    const splitPanes = activeBubble.querySelectorAll('.split-pane');
-    splitPanes.forEach(p => {
-        p.classList.add('hidden');
-    });
 
     setTimeout(() => {
         if(!activeBubble) return;
@@ -131,6 +110,20 @@ function onActiveBubbleClick(e, resetSideBubbles = true, overrideAnimatingCheck 
         isAnimating = false;
     }, animationTime * 1100);
 }
+//#endregion event handlers
+
+//#region helpers
+function createOverlay(){
+    const divOverlay = document.createElement('div');
+    divOverlay.classList.add('overlay');
+    document.body.appendChild(divOverlay);
+    divOverlay.addEventListener('click', onActiveBubbleClick);
+}
+function removeOverlay(){
+    const overlay = document.querySelector('.overlay');
+    if(overlay) overlay.remove();
+}
+
 
 function clearAnimations(){
     const bubbles = document.querySelectorAll('.side-bubble, .center-bubble');
@@ -140,6 +133,13 @@ function clearAnimations(){
         children.forEach(child => {
             child.style.animation = '';
         });
+    });
+}
+function animateOnSetActiveBubble(){
+    const children = activeBubble.querySelectorAll('.bubble-title');
+    activeBubble.style.animation = `zoom-in ${animationTime}s ease-out`;
+    children.forEach(child => {
+        child.style.animation = `move-to-top ${animationTime}s ease-out`;
     });
 }
 
@@ -186,3 +186,4 @@ function stopAnimateRelatedBubble(text){
         relatedBubble.element.style.color = '';
     }
 }
+// #endregion Helpers
