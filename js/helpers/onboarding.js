cae7e8a06504b141c12e222e9893216c342f6686
Jeffrey Cornillie <jeffrey.cornillie@student.howest.be>
2026-06-08 22:02:40 +0200

    Add extra side bubble info

--- js/helpers/onboarding.js
+++ js/helpers/onboarding.js
@@ -1,6 +1,6 @@
 
 
-export function Initialize(){
+export function initialize(){
     const onboardingOverlay = document.querySelector('.onboarding');
     if(!sessionStorage.getItem('onboarded')){
         // Show onboarding content
@@ -8,9 +8,16 @@ export function Initialize(){
     }else{
         onboardingOverlay.classList.add('hidden');
     }
+    const resetButton = document.querySelector('.reset-onboarding');
+    resetButton.addEventListener('click', resetOnboarding);
 }
 export function completeOnboarding(){
     sessionStorage.setItem('onboarded', 'true');
     const onboardingOverlay = document.querySelector('.onboarding');
     onboardingOverlay.classList.add('hidden');
+}
+export function resetOnboarding(){
+    sessionStorage.removeItem('onboarded');
+    const onboardingOverlay = document.querySelector('.onboarding');
+    onboardingOverlay.classList.remove('hidden');
 }
